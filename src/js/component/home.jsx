1c0a0229bf447a37c1c5016825a646c401eecfc8
dani <danieljuhe@gmail.com>
2022-11-16 18:36:20 +0100

    start a api

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useEffect, useState } from "react";
 const Home = () => {
+
+	const [characters, setCharacters]=useState([]);
+
+	useEffect(()=>{
+		fetch('https://rickandmortyapi.com/api/character')
+		.then((response)=>{
+			return response.json()
+		})
+		.then((response)=>{
+			setCharacters(response.results)
+		})
+	},[])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row">
+				<div className="col">
+					{
+						characters.map((character,index)=>{
+							return <div class="card mb-3" style="max-width: 540px;" key={index}>
+										<div class="row g-0">
+											<div class="col-md-4">
+											<img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" class="img-fluid rounded-start" alt="..."/>
+											</div>
+											<div class="col-md-8">
+											<div class="card-body">
+												<h5 class="card-title">{character.name}</h5>
+												<p class="card-text">Specie:{character.species}</p>
+												<p class="card-text">Gender:{character.gender}</p>
+												<p class="card-text">Origin:{character.origin.name}</p>
+												<p class="card-text"><small class="text-muted">Status:{character.status}</small></p>
+											</div>
+											</div>
+										</div>
+									</div>
+						})
+					}
+				</div>
+			</div>
+
 		</div>
 	);
 };
